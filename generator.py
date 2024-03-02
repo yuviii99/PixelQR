7a4a10d2ce94b8711e186fba75c81205dbe31569
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-03-03 03:14:02 +1100

    Update qr generator

--- generator.py
+++ generator.py
@@ -11,12 +11,11 @@ def generate_qr(file_path, event_name, qr_uuid_list):
 
 def generateUUID(event_name):
     id = uuid.uuid4()
-    print(type(id))
     return event_name + "-" + str(id)
 
 
 if __name__ == '__main__':
-    event_name = input("Enter the number of event: ").strip().replace(" ", "_")
+    event_name = input("Enter the name of event: ").strip().replace(" ", "_")
     qr_quantity = int(input("Enter the number of QR you want to generate: "))
     qr_uuid_list = []
     for i in range(qr_quantity):
