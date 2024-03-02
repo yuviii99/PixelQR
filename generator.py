832f09775c9d25ced9acc16041c304ccee074632
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-03-03 00:09:28 +1100

    Added QR Code generator using uuid4

---
+++ generator.py
@@ -0,0 +1,25 @@
+import segno
+import uuid
+import os
+
+
+def generate_qr(file_path, event_name, qr_uuid_list):
+    for uuid in qr_uuid_list:
+        qr_code = segno.make_qr(uuid)
+        qr_code.save(f"{file_path}/{event_name}/{uuid}.png")
+
+
+def generateUUID(event_name):
+    id = uuid.uuid4()
+    print(type(id))
+    return str(id) + "_" + event_name
+
+
+if __name__ == '__main__':
+    event_name = input("Enter the number of event: ").strip().replace(" ", "_")
+    qr_quantity = int(input("Enter the number of QR you want to generate: "))
+    qr_uuid_list = []
+    for i in range(qr_quantity):
+        qr_uuid_list.append(generateUUID(event_name))
+    os.makedirs(f"qr_codes/{event_name}")
+    generate_qr(os.getcwd() + "/qr_codes", event_name, qr_uuid_list)
